4f74bca0042a9e29f8fa60354104e9860a15bb9d
marwa Radwan <marwaradwan872@hotmail.com>
2021-08-09 13:42:20 +0200

    s2

--- session2/todaApp/js/custom.js
+++ session2/todaApp/js/custom.js
@@ -1,18 +1,31 @@
 const showHidebtn = document.querySelector('#showHidebtn button')
 const myForm = document.querySelector('#myForm')
 const formData = document.querySelector('#myForm form')
+const mainDataHeads = ['taskTitle', 'taskContent', 'taskType']
+let tasks = []
+const getTasks = () =>{
+    tasks = localStorage.getItem('tasks') || '[]'
+    return JSON.parse(tasks)
+}
+const setTasks = (tasks) =>{
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 showHideEvent = function(e){
     myForm.classList.toggle('d-none')
-    this.innerText === "show form"?  this.innerText = "hide form" : this.innerText = "show form"
+    if(e.target) this.innerText === "show form"?  this.innerText = "hide form" : this.innerText = "show form"
+    else{ e.innerText="show form" }
 }
 addTask = function(e){
     e.preventDefault()
-    task = {
-        status:false,
-        id : new Date().getTime()
-    }
-    alert(`${task.id} - ${task.status}`)
+    task = { status:false, id : new Date().getTime()}
+    mainDataHeads.forEach( head => task[head] = this.elements[head].value );
+    tasks = getTasks()
+    tasks.push(task)
+    setTasks(tasks)
+    this.reset()
+    showHideEvent(showHidebtn)
 }
 
 showHidebtn.addEventListener('click', showHideEvent )
 formData.addEventListener('submit', addTask)
+
