c69732b4296d9f05e93b91a49f2ee22042633b08
marwa Radwan <marwaradwan872@hotmail.com>
2021-08-11 10:21:50 +0200

    s4

--- session4/students.js
+++ session4/students.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
+const classesList = ['A', 'B', 'C', 'D']
+
 const readStudentFromJSON = () => {
     let students
     try{
@@ -17,6 +19,21 @@ const writeStudentsInJSON = (data)=>{
     fs.writeFileSync('s.json', JSON.stringify(data))
 }
 
-const addStudent = (studentData) =>{
-
+const generateID =(allStudents) =>{
+    let id = 1
+    try{ id = allStudents[allStudents.length-1].id+1 }
+    catch(e){}
+    return id;
 }
+const addStudent = (studentData) =>{
+    let allStudents = readStudentFromJSON()
+    if( classesList.findIndex(c=> c == studentData.class.toUpperCase()) == -1 ) return console.log(chalk.red('invalid class name'))
+    const student = {
+        id : generateID(allStudents),
+        name: studentData.name,
+        class: studentData.class.toUpperCase()
+    }
+    console.log(student)
+}
+
+addStudent({class:"b"})
