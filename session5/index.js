ad76a41e1bdf89c54079e33c175a4ea40608a32c
marwa Radwan <marwaradwan872@hotmail.com>
2021-08-12 10:45:40 +0200

    s5

--- session5/index.js
+++ session5/index.js
@@ -3,19 +3,32 @@ npm init --y
 npm i -g nodemon
 npm i express
 nodemon index
+npm i hbs
  */
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 const publicFiles = path.join(__dirname, '/public')
+const viewsFiles = path.join(__dirname, '/myfront/views')
+const layoutFiles = path.join(__dirname, '/myfront/layouts')
 
 app.use(express.static(publicFiles))
+app.set('view engine' , 'hbs')
+
+app.set('views', viewsFiles)
+
+hbs.registerPartials(layoutFiles)
 //route
+
 // app.get('', (req, res)=>{
     // res.send({a:1, b:2})
 //     res.sendFile(`${publicFiles}/about.html`)
 // })
+app.get('', (req,res)=>{
+    res.render('home')
+})
 
 app.listen(3000, ()=> console.log('server up'))
 //localhost:3000
