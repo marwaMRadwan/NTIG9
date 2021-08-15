f51a581261df2d8b7662657e5fcff998f5c9f5af
marwaMRadwan <marwaradwan872@hotmail.com>
2021-08-15 10:29:02 +0200

    s6

--- session6/src/controller/user.controller.js
+++ session6/src/controller/user.controller.js
@@ -12,17 +12,35 @@ const saveJsonFile = () =>{
 }
 class User{
     addNewUser(name, balance, age){
-        data = readJsonFile()
+        readJsonFile()
         let newUser = {
             _id:new Date().now(),
             name,balance, age
         }
         data.push(newUser)
+        saveJsonFile()
     }
-    editUser(){}
-    showAllUsers(){}
-    searchUser(){}
-    deleteUser(){}
+    editUser(userId, newData){
+        readJsonFile()
+        let index = data.findIndex(user=> user._id === userId)
+        data[index] = newData
+        saveJsonFile()
+    }
+    showAllUsers(){
+        readJsonFile()
+        return data
+    }
+    searchUser(userId){
+        readJsonFile()
+        let index = data.findIndex(user=> user._id === userId)
+        return index
+    }
+    deleteUser(userId){
+        readJsonFile()
+        let index = data.findIndex(user=> user._id === userId)
+        data.splice(index, 1)
+        saveJsonFile()
+    }
 }
 
 const userObj = new User()
