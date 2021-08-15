8ed0e16bca277ba8c6dfd6057837340b536d1c07
marwaMRadwan <marwaradwan872@hotmail.com>
2021-08-15 13:16:24 +0200

    s6

--- session6/routes/user.routes.js
+++ session6/routes/user.routes.js
@@ -1,18 +1,26 @@
 const express=require('express')
 const router = express.Router()
 const userController  = require('../src/controller/user.controller')
-
+router.get("",(req,res)=>{
+    res.redirect('/showAll')
+})
 router.get('/add', (req,res)=>{
-    if(req.query.name){
-        userController.addNewUser(req.query.name, req.query.balance, req.query.age)
-
-    }
-    res.render('add')
+    res.render('add', {title: "add new user"})
 })
 
 router.post('/add', (req,res)=>{
     console.log(req.body)
     userController.addNewUser(req.body.name, req.body.balance, req.body.age)
-    res.redirect('/add')
+    res.redirect('/showAll')
+})
+router.get('/showAll', (req,res)=>{
+    allusers= userController.showAllUsers()
+    console.log( allusers.length )
+    console.log(allusers)
+    res.render('all', {
+        title:"all Data",
+        allusers,
+        isEmpty: allusers.length?false:true
+    })
 })
 module.exports = router
