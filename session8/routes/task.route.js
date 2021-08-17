1054d123f3b149e009ea6ce833b2a00a997ea96f
marwaMRadwan <marwaradwan872@hotmail.com>
2021-08-17 15:06:43 +0200

    s8

--- session8/routes/task.route.js
+++ session8/routes/task.route.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = new express.Router()
 const dataController = require('../app/controller/data.controller')
-router.post('/',dataController.addUser )
+// router.post('/',dataController.addUser )
 
-router.get('/allTasks', dataController.getAllTasks)
-router.get('/alLTasks/:id', dataController.getSingle)
+// router.get('/allTasks', dataController.getAllTasks)
+// router.get('/alLTasks/:id', dataController.getSingle)
+router.get('/testR', (req,res)=>{
+    res.send('test')
+})
 module.exports = router
--- session8/src/server.js
+++ session8/src/server.js
@@ -4,6 +4,20 @@ const app = express()
 const dRoutes = require('../routes/task.route')
 const uRoutes = require('../routes/user.route')
 app.use(express.json())
-app.use(dRoutes)
-app.use('/user', uRoutes)
+
+// app.use(dRoutes)
+// app.use('/user', uRoutes)
+x = [
+    {a:'/user', r: uRoutes},
+    {a:'/', r: dRoutes}
+]
+x.forEach(element => { app.use( element.a, element.r ) });
+
+// let routes=[
+//     ['',require('../routes/users')]
+// ]
+// routes.forEach(r=>{
+//     app.use(...r)
+// })
+
 module.exports = app
