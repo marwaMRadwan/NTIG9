ffabab084c9670f8319a0393812cabb9ac386068
marwaMRadwan <marwaradwan872@hotmail.com>
2021-08-26 14:11:09 +0200

    s15

--- session15/src/app/pages/register/register.component.ts
+++ session15/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _user:UsersService) { }
 
   ngOnInit(): void {
   }
 
   handleSubmit(myData:any){
     console.log(myData.value)
+    this._user.registerUser(myData.value).subscribe(data=>{
+      console.log(data)
+    })
   }
 }
