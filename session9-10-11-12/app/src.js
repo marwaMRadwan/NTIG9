9a85cf64232345f0f066389e42f9ceffdf156a79
marwaMRadwan <marwaradwan872@hotmail.com>
2021-08-31 09:29:17 +0200

    s18

--- session9-10-11-12/app/src.js
+++ session9-10-11-12/app/src.js
@@ -5,6 +5,8 @@ require('./db/connection')
 
 const userRoutes = require('../routes/user.routes')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(userRoutes)
